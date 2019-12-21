32fece474a70c125505b3b5419c503ba10f2d7b9
Davids.berz <Davids.berz@gmail.com>
2019-12-21 13:13:52 -0600

    xs

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -8,7 +8,18 @@ import { Cliente } from './cliente';
 export class ClientesComponent  {
 
   clientes:Cliente[]=[
-    {id:1,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'}
+    {id:1,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:2,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:3,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:4,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:5,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:6,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:7,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:8,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:9,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:10,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:11,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
+    {id:12,nombre:'david',apellido:'saldivar',email:'david@cliente.com',createAt:'2018-12-11'},
   ];
 
 
