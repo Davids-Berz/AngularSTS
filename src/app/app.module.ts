5c8b8a05720a26a5924eb23dadac3064bff02594
Davids.berz <Davids.berz@gmail.com>
2019-12-23 17:37:10 -0600

    curso 32

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService} from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes',pathMatch:'full' },
@@ -32,6 +33,7 @@ export class AppRoutingModule {}
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
