3fa8788969f3b50c090665868b04824a68e1dbed
Davids.berz <Davids.berz@gmail.com>
2020-01-01 08:42:06 -0600

    curso 77

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FormComponent } from './clientes/form.component';
 import { FormsModule } from '@angular/forms';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es-MX';
+import { PaginatorComponent } from './paginator/paginator.component';
 
 
 
@@ -23,6 +24,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/clientes',pathMatch:'full' },
   { path: 'directivas', component: DirectivaComponent },
   { path: 'clientes', component: ClientesComponent },
+  { path: 'clientes/page/:page', component: ClientesComponent },
   { path: 'clientes/form', component: FormComponent},
   { path: 'clientes/form/:id', component: FormComponent}
 ];
@@ -30,7 +32,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  declarations: [FormComponent]
+  declarations: [FormComponent, PaginatorComponent]
 })
 export class AppRoutingModule {}
 
